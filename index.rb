80f695c1cd46df1529af99c05c003eca0093e5fc
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-09-28 01:48:04 +0300

    Add word and sentence decoders

--- index.rb
+++ index.rb
@@ -13,3 +13,16 @@ class MorseCodeDecoder
   def self.decode_char(morse_char)
     MORSE_CODE.key(morse_char)
   end
+
+  def self.decode_word(morse_word)
+    morse_word.split.map { |morse_char| decode_char(morse_char) }.join
+  end
+
+  def self.decode_message(morse_message)
+    morse_message.split('   ').map { |morse_word| decode_word(morse_word) }.join(' ')
+  end
+end
+
+message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+decoded_message = MorseCodeDecoder.decode_message(message)
+puts decoded_message
